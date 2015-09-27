c0e9e47307a8ab2add746ad8826960abd9d7c430
Satyender Yadav <satyenderyadav4993@gmail.com>
2015-09-27 16:37:12 +0530

    Client Code

---
+++ app.js
@@ -0,0 +1,29 @@
+var socketIOClient = require('socket.io-client');
+var sailsIOClient = require('sails.io.js');
+
+// Instantiate the socket client (`io`)
+// (for now, you must explicitly pass in the socket.io client when using this library from Node.js)
+var io = sailsIOClient(socketIOClient);
+
+// Set some options:
+// (you have to specify the host and port of the Sails backend when using this library from Node.js)
+io.sails.url = 'http://localhost:1337';
+// ...
+
+// Send a GET request to `http://localhost:1337/hello`:
+io.socket.get('/joinrpi', function serverResponded (body, JWR) {
+  // body === JWR.body
+  console.log('Sails responded with: ', body);
+  console.log('with headers: ', JWR.headers);
+  console.log('and with status code: ', JWR.statusCode);
+
+  io.socket.on('deployedMessage',function(data){
+    console.log(data);
+  });
+
+  // When you are finished with `io.socket`, or any other sockets you connect manually,
+  // you should make sure and disconnect them, e.g.:
+  //io.socket.disconnect();
+
+  // (note that there is no callback argument to the `.disconnect` method)
+});
